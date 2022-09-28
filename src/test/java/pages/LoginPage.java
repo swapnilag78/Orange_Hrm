6b4369f2e15db3d8e4ee9c1469666b22ba020bf0
swapnil.agrawal <swapnil.ag2001@gmail.com>
2022-09-28 23:56:45 +0530

    automation completed

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,31 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class loginPage {
+	WebDriver driver;
+
+	By email=By.id("txtUsername");
+	By password= By.id("txtPassword");
+	By login=By.xpath("//div//button");
+	
+	public loginPage(WebDriver driver) {
+		this.driver= driver;
+		System.out.println(driver.getTitle());
+		
+	}
+	
+
+//	public dashboardPage login(String user, String pass) throws InterruptedException{
+//		driver.findElement(email).sendKeys(user);
+//		driver.findElement(password).sendKeys(pass);
+//		driver.findElement(login).click();
+//		Thread.sleep(3000);
+//		return new dashboardPage(driver);
+//		
+//		
+//	}
+	
+	
 }
--- src/test/java/test_cases/baseTest.java
+++ src/test/java/test_cases/baseTest.java
@@ -1,5 +1,37 @@
 package test_cases;
 
-public class baseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.loginPage;
 
+public class baseTest {
+	public static WebDriver webdriver_driver;
+	loginPage loginpage_obj;
+	@BeforeClass
+	public void setupwindow() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		webdriver_driver = new ChromeDriver();
+		
+		
+		webdriver_driver.manage().window().maximize();
+		webdriver_driver.get("https://sanjay29-trials76.orangehrmlive.com/");
+		Thread.sleep(4000);
+		System.out.println("Login Page is Opened!");
+		
+		loginpage_obj = new loginPage(webdriver_driver);
+		System.out.println("Opening the browser");
+		loginpage_obj.login("Admin", "XwFwrZ28@Y");
+		Thread.sleep(2000);
+		
+		
+	}
+
+	
+	@AfterClass
+	public void close() {
+		webdriver_driver.close();
+	}
 }
